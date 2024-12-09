ed1ee761d3051b6dba9762dde6c2b9795b69aa18
Nathan Waivio <nathan.waivio@gmail.com>
2024-12-08 21:01:17 -0700

    Parallel lately,  everything seems good.

--- src/Posit.hs
+++ src/Posit.hs
@@ -857,14 +857,14 @@ approx_cosh x = (approx_exp x + approx_exp (negate x))/2
 
 approx_asinh :: PositC es => Posit es -> Posit es
 approx_asinh NaR = NaR
-approx_asinh x = approx_log $ x + approx_sqrt (fma x x 1)  -- (x^2 + 1)
+approx_asinh x = approx_log $ x + approx_sqrt (fma x x 1)  -- (x^2 + 1)  -- 
 
 
 approx_acosh :: PositC es => Posit es -> Posit es
 approx_acosh NaR = NaR
 approx_acosh x
   | x < 1 = NaR
-  | otherwise = approx_log $ x + approx_sqrt (fma x x (-1))  -- (x^2 - 1)
+  | otherwise = approx_log $ x + approx_sqrt (fma x x (-1))  -- (x^2 - 1)  -- 
 
 
 approx_atanh :: forall es. PositC es => Posit es -> Posit es
